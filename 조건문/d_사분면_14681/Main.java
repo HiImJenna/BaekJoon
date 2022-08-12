a84892cbdf5346b2f35115bfb9009e70ddd056e3
HiImJenna <skygarden316@naver.com>
2022-08-13 01:38:31 +0900

    2022_08_13_01:38

---
+++ 조건문/d_사분면_14681/Main.java
@@ -0,0 +1,32 @@
+package 조건문.d_사분면_14681;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        xy(sc.nextInt(), sc.nextInt());
+
+        sc.close();
+
+    }
+
+
+    public static void xy(int a, int b) {
+        if (a > 0 && b > 0){ 
+            System.out.println("1");
+        } else if (a < 0 && b > 0){
+            System.out.println("2"); 
+        } else if (a < 0 && b < 0) {
+            System.out.println("3");
+        } else if (a > 0 && b < 0) {
+            System.out.println("4");
+        }
+}
+}
+
+// 예를 들어, 좌표가 (12, 5)인 점 A는 x좌표와 y좌표가 모두 양수이므로 제1사분면에 속한다. 점 B는 x좌표가 음수이고 y좌표가 양수이므로 제2사분면에 속한다.
+
+// 점의 좌표를 입력받아 그 점이 어느 사분면에 속하는지 알아내는 프로그램을 작성하시오. 단, x좌표와 y좌표는 모두 양수나 음수라고 가정한다.
---
+++ 조건문/e_알람시계_2884/Main.java
@@ -0,0 +1,32 @@
+package 조건문.e_알람시계_2884;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        time (sc.nextInt(), sc.nextInt());
+
+        sc.close();
+
+    }
+        public static void time(int a, int b){
+            if (b >= 45){
+               System.out.print(a + " ");
+               System.out.println(b - 45);
+            } else if ( b < 45 ){
+                System.out.println(a - 1 + " ");
+                int c = 45 - b;
+                System.out.println(60 - c);
+            }
+        }
+}
+
+// 첫째 줄에 두 정수 H와 M이 주어진다. (0 ≤ H ≤ 23, 0 ≤ M ≤ 59) 그리고 
+// 이것은 현재 상근이가 설정한 놓은 알람 시간 H시 M분을 의미한다.
+
+// 입력 시간은 24시간 표현을 사용한다. 
+// 24시간 표현에서 하루의 시작은 0:0(자정)이고, 끝은 23:59(다음날 자정 1분 전)이다. 
+// 시간을 나타낼 때, 불필요한 0은 사용하지 않는다.
