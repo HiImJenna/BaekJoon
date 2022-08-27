d278b0a6a5c997f321ea6f3a337a6c299008ae6a
HiImJenna <skygarden316@naver.com>
2022-08-27 23:06:50 +0900

    2022.08.27.23:06

---
+++ C_반복문/k_X보다작은수_/Main.java
@@ -0,0 +1,29 @@
+package C_반복문.k_X보다작은수_;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lenght = sc.nextInt();
+        int max = sc.nextInt();
+        int arr[] = new int [lenght];
+
+        for (int i = 0; i < lenght; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int j = 0; j < lenght; j++) {
+            if (arr[j] < max){
+                System.out.print(arr[j] + " ");
+            }
+
+        }
+           
+        
+        sc.close();
+
+        }
+        
+    }
+
