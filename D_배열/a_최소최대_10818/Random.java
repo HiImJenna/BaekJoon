7e9712334671422cca6a830aa3a9dc6c9ab5fb86
JeongWonS <thswjddnjs0316@gmail.com>
2022-08-30 09:06:23 +0900

    2022.08.30.9:06

---
+++ D_배열/a_최소최대_10818/Random.java
@@ -0,0 +1,9 @@
+package D_배열.a_최소최대_10818;
+
+public class Random {
+
+    public static int nextInt(int i) {
+        return 0;
+    }
+
+}
---
+++ D_배열/b_최댓값_2562/Main.java
@@ -0,0 +1,5 @@
+package D_배열.b_최댓값_2562;
+
+public class Main {
+    
+}
